12135aa080405f441303efa1390437d5f4a3e4d7
dshaffer <dshaffer@udel.edu>
2025-03-16 14:19:00 -0400

    Edit mode complete and passes all tests

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,53 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): React.JSX.Element {
+    const [editMode, setEditMode] = useState<boolean>(false);
+    const toggleEditMode = () => {
+        setEditMode(!editMode);
+    };
+    const [userName, setUserName] = useState<string>("Your Name");
+    const [studentStatus, setStudentStatus] = useState<boolean>(true);
+    const toggleStudentStatus = () => {
+        setStudentStatus(!studentStatus);
+    };
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                id="edit-mode-switch"
+                label="Edit Mode"
+                checked={editMode}
+                onChange={toggleEditMode}
+            />
+            {editMode ?
+                <div>
+                    <Form.Group>
+                        <Form.Label>Name:</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={userName}
+                            onChange={(e) => {
+                                setUserName(e.target.value);
+                            }}
+                        ></Form.Control>
+                    </Form.Group>
+                    <Form.Check
+                        type="checkbox"
+                        id="student-status-checkbox"
+                        label="Is Student?"
+                        checked={studentStatus}
+                        onChange={toggleStudentStatus}
+                    />
+                </div>
+            :   <div>
+                    {studentStatus ?
+                        <div>{userName} is a student</div>
+                    :   <div>{userName} is not a student</div>}
+                </div>
+            }
         </div>
     );
 }
